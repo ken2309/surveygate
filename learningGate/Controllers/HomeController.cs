6de76af7fabc2cd755ece8fe256c46fecaba87da
NgQuangKhai <khai@saigon.digital>
2023-06-19 09:14:48 +0700

    [UPDATE] fix all

--- learningGate/Controllers/HomeController.cs
+++ learningGate/Controllers/HomeController.cs
@@ -35,6 +35,14 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
